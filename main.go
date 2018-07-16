3e61e91fd45ad0aec2bebd1883f3041611e33584
harshit777 <harshit.g.0702@gmail.com>
2018-07-16 22:52:56 +0530

    Structures

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+type person struct {
+	firstName string
+	lastName  string
+}
+
+func main() {
+	/*alex := person{firstName: "Alex", lastName: "Anderson"}
+	fmt.Println(alex)*/
+
+	var alex person
+
+	alex.firstName = "Alex"
+	alex.lastName = "Anderson"
+
+	fmt.Println(alex)
+	fmt.Printf("%+v", alex)
+
+}
